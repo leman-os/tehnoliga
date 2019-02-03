ff484472f5f173afbf9dd64bf5eb20b0bd8d8482
Gulin Sergey <info@wb-group.ru>
2019-02-03 19:55:24 +0300

    main-cart-contact-catalog2

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ gulp.task('js', ['common-js'], function() {
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/bxslider-4/dist/jquery.bxslider.min.js',
 		'app/libs/owl.carousel/dist/owl.carousel.min.js',
+		'app/libs/fancybox/dist/jquery.fancybox.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
